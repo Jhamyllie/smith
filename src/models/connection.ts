e47d110c0de85063e458be532c312ba13412e577
Jamile <milli.santana2017@gmail.com>
2022-09-20 15:15:05 -0300

    fix: correções e alterações no arquivo connection

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -3,10 +3,15 @@ import mysql from 'mysql2/promise';
 
 dotenv.config();
 
+// const connection = mysql.createPool({
+//   host: 'db',
+//   user: 'root',
+//   password: 'password',
+// });
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 export default connection;
